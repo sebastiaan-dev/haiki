53e8efd809b79b7187d4ba27dd7f7386973f21af
Mauro Eijsenring <mauro@eijsenring.com>
2024-03-09 04:57:04 +0100

    added route to api

--- article-api/main.py
+++ article-api/main.py
@@ -1,16 +1,22 @@
 from dotenv import load_dotenv
+from starlette.responses import JSONResponse
 
 from utils.files import get_folders_from_dir
 
 load_dotenv()
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 import pipelines as pl
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*"
+)
 
 @app.get("/article/{id}")
 def get_article():
@@ -49,3 +55,10 @@ def create_papers(paper: CreatePaper):
     # pl.papers(paper.path)
 
     return {"msg": "Request processed successfully"}
+
+@app.get("/topics")
+def get_topics():
+    """
+    Get all the hot topics
+    """
+    return JSONResponse(content=["Creatine", "Ayaska", "THC", "Ethanol"])
